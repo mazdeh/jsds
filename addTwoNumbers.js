e92fd245b10c21be1ca0eb9aee83c6544740c7c8
Vahid Mazde <vahid.mazdeh@fourwindsinteractive.com>
2016-11-18 09:01:08 -0700

    cleaner, final version of

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -1,26 +1,59 @@
-"use strict";
-var LinkedList = require('./linkedlist').LinkedList;
-
-
-var l1 = new LinkedList();
-
-// l1 is 321
-l1.add(1, 0);
-l1.add(2, 1);
-l1.add(3, 2);
-
-// l2 is 123
-var l2 = new LinkedList();
-l2.add(3, 0);
-l2.add(2, 1);
-l2.add(1, 2);
-
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
 var addTwoNumbers = function(l1, l2) {
-  return l2.head.value;
-
+    var sum = [],
+        carry = 0,
+        list1 = l1,
+        list2 = l2,
+        i = 0;
+    
+    while (list1 || list2) {
+      var p = ((list1) ? list1.val : 0);
+      var q = ((list2) ? list2.val : 0);  
+        
+        sum[i] = p + q + carry;
+        if (sum[i] >= 10) {
+          sum[i] = sum[i] % 10;
+          carry = 1;
+        } else {
+          carry = 0;
+        }
+        
+        if (list1) { list1 = list1.next}
+        if (list2) { list2 = list2.next}
+        i++;
+    }
+    if (carry > 0) {
+      sum[sum.length] = 1;
+    }
+    
+    return sum;
 };
 
+var l1 = null;
 
+var l2 = {
+  val: 5, 
+  next: null
+}
+
+console.log('l1: ', l1);
+console.log('l2: ', l2);
 var result = addTwoNumbers(l1, l2);
 console.log('result: ', result);
+
+
+
+
+
 
